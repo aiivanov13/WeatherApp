aa860e2b39cccade2934e52aee08eb9258bb409c
aivanov <alex130896@gmail.com>
2026-02-20 12:52:32 +0400

    Initial Commit

---
+++ WeatherApp/Modules/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Александр Иванов on 20.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
